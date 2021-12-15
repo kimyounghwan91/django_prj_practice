d9ae833849f09d4386ad29c827ae5cdd5f11b57f
kimyounghwan91 <youthhk0@gmail.com>
2021-12-15 17:25:18 +0900

    진행상태 업로드

--- member/models.py
+++ member/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class Member(models.Model):
+    name = models.CharField(max_length=30)
+    age = models.IntegerField()
+    birth = models.DateField()
+    email = models.EmailField()
+
+
+
+    def __str__(self):
+        return f'[{self.pk}] {self.name}'
+
+
+    def get_absolute_url(self):
+        return f'/member/{self.pk}/'
--- member/views.py
+++ member/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
+from .models import  Member
 
-# Create your views here.
+def index(request):
+    members = Member.objects.all().order_by('pk')
+
+
+    return render(
+        request,
+        'member/index.html',
+        {
+            'members': members,
+        }
+    )
+def single_member_page(request, pk):
+    member = Member.objects.get(pk=pk)
+
+    return render(
+        request,
+        'member/single_member_page.html',
+        {
+            'member' : member,
+        }
+    )
